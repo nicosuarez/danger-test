c72134702a342fff106bf7dc1061f41458372ca7
Nicolas Andres Suarez <nicolasandres.suarez@mercadolibre.com>
2016-05-29 00:53:27 -0300

    Fix error

--- Sources/TestClass.m
+++ Sources/TestClass.m
@@ -5,7 +5,7 @@
 {
 	self = [super init];
 	if (self) {
-		self.name = [dictionary objectForKey:@"available_balance"];
+		self.name = [dictionary ml_objectForKey:@"available_balance"];
 		self.surname = [dictionary ml_objectForKey:@"available_balance"];
 	}
 	return self;
